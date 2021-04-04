7d808addabd8877bf321bab2d5d9fb82b8cc7008
Ferby <ferby@Ferbys-MacBook-Air.local>
2021-04-04 23:28:53 +1200

    programming

---
+++ sample-generator.js
@@ -0,0 +1,173 @@
+// -- SYSTEM -- //
+
+const YOUTUBE_API_KEY = "AIzaSyD3bLEhF37F9PsiHbkVfipqCQg6f7jfGuw";
+
+var searchText = document.getElementById("search-text");
+
+var loadingAudio = new Audio('./audio/loading.wav');
+loadingAudio.loop = true;
+
+var finishedLoading = new Audio('./audio/finishedLoading.wav');
+
+window.addEventListener('load', (event) => {
+    document.getElementById("anything").onclick = generateRandomWord;
+    document.getElementById("betterthing").onclick = getBetterResult;
+    document.getElementById("secret-button").onclick = activateSecretButton;
+});
+
+// -- GET A RANDOM VIDEO -- //
+
+function generateRandomWord() {
+
+    StartLoadingIcon()
+
+    fetch("https://random-word-api.herokuapp.com/word?number=1")
+    .then(response => response.json())
+    .then(data => {
+
+            console.log(data[0])
+
+            var word = data[0]
+            var mainWord = mainWords[Math.floor(Math.random() * (mainWords.length-1))]
+            var keyWords = `${word}%20${mainWord}` 
+
+            searchText.innerHTML = `Searching for ${word} and ${mainWord}.`
+
+            const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${keyWords}&key=${YOUTUBE_API_KEY}`;
+
+            fetch(url)
+            .then(res => res.json())
+            .then(data2 => {
+            document.getElementById("youtubeVideo").src = `https://www.youtube.com/embed/${data2.items[0].id.videoId}`;
+            StopLoadingIcon();
+        });
+    })
+}
+
+// -- GET BETTER VIDEO -- //
+
+function getSearchKeyWords() {
+    var word1 = searchWords[Math.floor(Math.random() * (searchWords.length-1))]
+    var word2 = searchWords[Math.floor(Math.random() * (searchWords.length-1))]
+    var mainWord = mainWords[Math.floor(Math.random() * (mainWords.length-1))]
+    var keyWords = `${word1}%20${word2}%20${mainWord}` 
+    searchText.innerHTML = `Searching for ${word1}, ${word2} and ${mainWord}.`
+    return keyWords
+}
+
+var mainWords = ["music", "sample", "audio", "sound", "song"]
+
+var searchWords = ["latin", "gay", "guitar", "robot", "surf", "emo", "terrible", "meme", "german", "scream", "animal", "movie", "video", "videogame"
+, "rap", "hiphop", "ocean", "darkness", "cartoon", "upsetting", "synth", "bells", "strings", "drums", "impressive", "acapella", "singing", "voice", "freestyle",
+"jazz", "funk", "vinyl", "bassline", "old", "1950s", "2000s", "1920s", "2010", "1990s", "children", "baby", "machinery", "horror", "love", "death", "funeral",
+"gentle", "hippy", "soul", "spy", "classical", "piano", "harp", "beat", "sexy", "underground", "edgy", "westcoast", "feet", "caveman", "christian", "country", "metal", "rock", "japanese",
+"korean", "chinese", "highschool", "teen", "worst", "best", "six", "pop", "soundeffect", "technology", "computer", "psychic", "sad", "lesbian", "transgender", "bisexual"]
+
+
+function getBetterResult() {
+
+    StartLoadingIcon()
+  
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${getSearchKeyWords()}&key=${YOUTUBE_API_KEY}`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+      var newUrl = `https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=${getVideoIDStringsFromArray(data.items)}&key=${YOUTUBE_API_KEY}`
+      fetch(newUrl)
+      .then(response => response.json())
+      .then(data2 => {
+
+        var foundOne = false;
+
+        var attempts = 50
+
+        while (!foundOne) {
+            var video = data2.items[Math.floor(Math.random() * (data2.items.length-1))]
+
+            if (convert_time(video.contentDetails.duration) < 600) {
+                document.getElementById("youtubeVideo").src = `https://www.youtube.com/embed/${video.id}`;
+                StopLoadingIcon();    
+                foundOne = true
+            }
+
+            attempts--
+
+            if (attempts <= 0)
+                getBetterResult()
+        }
+      })
+  });
+}
+
+// -- SECRET -- // 
+
+function activateSecretButton(e) {
+    e.target.classList.add("rainbow")
+    e.target.innerHTML = "♡"
+    e.target.color = "Red"
+    e.target.style.fontSize = "40pt"
+    var audio = new Audio('./audio/secret.wav');
+    document.getElementById("search-text").style.display = "block"
+    audio.play();
+}
+
+// -- UTILITY -- //
+
+function getVideoIDStringsFromArray(items) {
+    var str = ''
+
+    for (var i = 0; i < items.length; i++) {
+        str += items[i].id.videoId
+
+        if (i < items.length - 1)
+            str += "%2C"
+    }
+
+    return str
+}
+
+function convert_time(duration) {
+    var a = duration.match(/\d+/g);
+
+    if (duration.indexOf('M') >= 0 && duration.indexOf('H') == -1 && duration.indexOf('S') == -1) {
+        a = [0, a[0], 0];
+    }
+
+    if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1) {
+        a = [a[0], 0, a[1]];
+    }
+    if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1 && duration.indexOf('S') == -1) {
+        a = [a[0], 0, 0];
+    }
+
+    duration = 0;
+
+    if (a.length == 3) {
+        duration = duration + parseInt(a[0]) * 3600;
+        duration = duration + parseInt(a[1]) * 60;
+        duration = duration + parseInt(a[2]);
+    }
+
+    if (a.length == 2) {
+        duration = duration + parseInt(a[0]) * 60;
+        duration = duration + parseInt(a[1]);
+    }
+
+    if (a.length == 1) {
+        duration = duration + parseInt(a[0]);
+    }
+    return duration
+}
+
+function StartLoadingIcon() {
+    loadingAudio.play()
+    document.getElementById("loading-icon").style.display = "block"
+  }
+
+  function StopLoadingIcon() {
+      loadingAudio.pause()
+    document.getElementById("loading-icon").style.display = "none"
+    finishedLoading.play();
+  }
